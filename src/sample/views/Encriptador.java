c88d9085c9c6d49d61817688b83d26cb1b1a287d
isctorres <isctorres@gmail.com>
2021-04-12 18:09:49 -0500

    Clase 11

--- src/sample/views/Encriptador.java
+++ src/sample/views/Encriptador.java
@@ -1,14 +1,19 @@
 package sample.views;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Encriptador extends Stage {
+public class Encriptador extends Stage implements EventHandler<KeyEvent> {
 
     private Scene escena;
     private HBox hBox;
@@ -17,6 +22,8 @@ public class Encriptador extends Stage {
     private TextArea txtEntrada;
     private TextArea txtSalida;
     private Button btnEncriptar;
+    private Button btnTolAbrir;
+    private FileChooser fileChooser;
 
     public Encriptador(){
 
@@ -30,13 +37,50 @@ public class Encriptador extends Stage {
     private void CrearUI() {
         vBox = new VBox();
         tolMenu = new ToolBar();
+        btnTolAbrir = new Button();
+        ImageView imv = new ImageView("sample/assets/icon_open.png");
+        imv.setFitHeight(35);
+        imv.setPreserveRatio(true);
+        btnTolAbrir.setGraphic(imv);
+        btnTolAbrir.setOnAction(event -> abrirArchivo());
+        tolMenu.getItems().add(btnTolAbrir);
+
         hBox = new HBox();
         txtEntrada = new TextArea();
+        txtEntrada.setOnKeyPressed(this);
+
         txtSalida = new TextArea();
+        txtSalida.setEditable(false);
+
         hBox.getChildren().addAll(txtEntrada,txtSalida);
         btnEncriptar = new Button("Encriptar Entrada");
+        btnEncriptar.setOnAction(event -> encriptarEntrada());
         vBox.getChildren().addAll(tolMenu,hBox,btnEncriptar);
 
-        escena = new Scene(vBox,300,450);
+        escena = new Scene(vBox,600,300);
+    }
+
+    private void encriptarEntrada() {
+    }
+
+    private void abrirArchivo() {
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar archivo a encriptar :)");
+        fileChooser.showOpenDialog(this);
+    }
+
+    @Override
+    public void handle(KeyEvent event) {
+
+        //txtSalida.appendText(event.getCode().ordinal()+"");
+
+        switch(event.getCode().toString()){
+            case "A":
+                txtSalida.appendText("b"); break;
+            case "B":
+                txtSalida.appendText("c");
+                break;
+
+        }
     }
 }
